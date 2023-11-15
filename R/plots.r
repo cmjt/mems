baf5241a9346dcc834a608fa2f58cdd102a5a3c7
cmjt <cmjonestodd@gmail.com>
2023-11-15 16:24:38 +1300

    plotting of contributions

--- R/plots.r
+++ R/plots.r
@@ -1,5 +1,6 @@
 #' Plots the calculated metrics from a \code{mems} output
 #' (i.e., each triangle metric).
+#' @param x output of \code{mems::mems}
 #' @return A  \code{"patchwork"}, \code{"gg"}, \code{"ggplot"} object.
 #' @export
 gg_mems <- function(x){
@@ -27,26 +28,107 @@ gg_mems <- function(x){
         ggplot2::theme_void()
 }
 #' Plots Q blocks contribution of nodes
-gg_qblocks <- function(mesh, sf = TRUE){
-    sf <- mesh_2_sf(mesh)
-    qb <- q_blocks(mesh)
-    df <- data.frame(ID = 1:nrow(sf), area = qb$triangle_area)
-    sf <- dplyr::left_join(sf, df, by = "ID")
-    mids <- cens(mesh) ## center of triangles
-    plt_C0 <- ggplot2::ggplot(sf) + ggplot2::geom_sf(ggplot2::aes(fill = area)) + theme_void() +
-        ggplot2::geom_text(data = mids, ggplot2::aes(x = x, y = y, label = triangle), inherit.aes = FALSE) +
-        geom_point(data = data.frame(x = mesh$loc[ , 1], y = mesh$loc[ , 2]),
-                   ggplot2::aes(x = x, y = y, size = diag(as.matrix(qb$C0)))) +
-        labs(fill = "Area", size = "C0")
-    plt_G1 <- ggplot2::ggplot(sf) + ggplot2::geom_sf(ggplot2::aes(fill = area)) + theme_void() +
-        ggplot2::geom_text(data = mids, ggplot2::aes(x = x, y = y, label = triangle), inherit.aes = FALSE) +
-        geom_point(data = data.frame(x = mesh$loc[ , 1], y = mesh$loc[ , 2]),
-                   ggplot2::aes(x = x, y = y, size = rowSums(abs(as.matrix(qb$G1))))) +
-        labs(fill = "Area", size = "G1")
-    plt_B1 <- ggplot2::ggplot(sf) + ggplot2::geom_sf(ggplot2::aes(fill = area)) + theme_void() +
-        ggplot2::geom_text(data = mids, ggplot2::aes(x = x, y = y, label = triangle), inherit.aes = FALSE) +
-        geom_point(data = data.frame(x = mesh$loc[ , 1], y = mesh$loc[ , 2]),
-                   ggplot2::aes(x = x, y = y, size = rowSums(abs(as.matrix(qb$B1))))) +
-        labs(fill = "Area", size = "B1")
-     patchwork::wrap_plots(plt_C0, plt_G1,plt_B1, nrow = 3)
+#' only useful for small number of nodes
+#' @inheritParams mems
+#' @export
+gg_qblocks <- function(mesh){
+    fm <- fmesher::fm_fem(mesh) ## mems q_block to test
+    nv <- mesh$n
+    ## initialise matricies
+    matc1 <- matg1 <- matb1 <- matrix(NA, ncol = nv, nrow = nv)
+    ## C1
+    matc1[cbind((fm$c1@i + 1), (fm$c1@j + 1))] <- fm$c1@x 
+    c1.df <- reshape2::melt(matc1, c("x", "y"), value.name = "z")
+    plt_C1 <- ggplot(data = c1.df, aes(x = y,y = -x,fill = z)) +
+        geom_tile(size = 1.5, alpha = 0.8) + 
+        scale_x_continuous(breaks = seq(1, nv, 1), labels = paste("n",1:nv,sep = ""),
+                           expand = c(0, 0), position = "top") +
+        scale_y_continuous(breaks = -nv:-1,labels = paste("n",nv:1,sep = ""), expand = c(0, 0)) +
+        labs(fill = "C1") + xlab("") + ylab("") +
+        geom_text( aes(label = round(z, 3)), color="black", size=rel(4)) +
+        ggtitle("C1")
+    ## G1
+    matg1[cbind((fm$g1@i + 1), (fm$g1@j + 1))] <- fm$g1@x 
+    g1.df <- reshape2::melt(matg1, c("x", "y"), value.name = "z")
+    plt_G1 <- ggplot(data = g1.df, aes(x = y,y = -x,fill = z)) +
+        geom_tile(size = 1.5, alpha = 0.8) + 
+        scale_x_continuous(breaks = seq(1, nv, 1), labels = paste("n",1:nv,sep = ""),
+                           expand = c(0, 0), position = "top") +
+        scale_y_continuous(breaks = -nv:-1,labels = paste("n",nv:1,sep = ""), expand = c(0, 0)) +
+        labs(fill = "G1") + xlab("") + ylab("") +
+        geom_text( aes(label = round(z, 3)), color="black", size=rel(4)) +
+        ggtitle("G1")
+
+    ## B1
+    matb1[cbind((fm$b1@i + 1), (fm$b1@j + 1))] <- fm$b1@x 
+    b1.df <- reshape2::melt(matb1, c("x", "y"), value.name = "z")
+    plt_B1 <- ggplot(data = b1.df, aes(x = y,y = -x,fill = z)) +
+        geom_tile(size = 1.5, alpha = 0.8) + 
+        scale_x_continuous(breaks = seq(1, nv, 1), labels = paste("n",1:nv,sep = ""),
+                           expand = c(0, 0), position = "top") +
+        scale_y_continuous(breaks = -nv:-1,labels = paste("n",nv:1,sep = ""), expand = c(0, 0)) +
+        labs(fill = "B1") + xlab("") + ylab("") +
+        geom_text( aes(label = round(z, 3)), color="black", size=rel(4)) +
+        ggtitle("B1")
+
+    ## plot all matricies
+    patchwork::wrap_plots(plt_C1, plt_G1,plt_B1, nrow = 1) & 
+        scale_fill_continuous(type = "viridis", na.value = NA) &
+        theme(panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank(),
+              panel.border = element_rect(fill = NA,color = "gray90", size = 0.5, linetype = "solid"),
+              axis.line = element_blank(),
+              axis.ticks = element_blank(),
+              panel.background = element_rect(fill="gray90"),
+              plot.background = element_rect(fill="gray90"),
+              legend.position = "none", 
+              axis.text = element_text(color="black", size=10),
+              plot.title = element_text(size = 14))
+}
+#' Plot segment influence
+#' only useful for small number of nodes
+#' @inheritParams mems
+#' @export
+gg_seg_influence <- function(mesh){
+    nv <- mesh$n
+    nodes <- data.frame(x = mesh$loc[,1], y = mesh$loc[,2])
+    fm <- fmesher::fm_fem(mesh)
+    ## diag elements
+    nodes$valc1 <- Matrix::diag(fm$c1)
+    nodes$valg1 <- Matrix::diag(fm$g1)
+    nodes$valb1 <- Matrix::diag(fm$b1)
+    segs <- half_segments(mesh)
+    ## Voronoi
+    tesselation <- deldir::deldir(nodes$x, nodes$y)
+    tiles <- deldir::tile.list(tesselation) 
+    vor <- deldir_2_sf(tiles)
+    ## label points for line segments
+    crd <- sf::st_coordinates(segs)
+    lst <- split.data.frame(crd[, 1:2], crd[,3])
+    cens <- lapply(lst, function(x) mid(x[1,], x[2,]))
+    cents <- as.data.frame(do.call('rbind', cens))
+    ## dataframe C1
+    dfc1 <- data.frame(row = (fm$c1@i + 1), col = (fm$c1@j + 1), val = fm$c1@x)
+    ## remove off diag elements
+    dfc1 <- dfc1[-which(dfc1$row - dfc1$col == 0),]
+    plt_C1 <- ggplot() + geom_sf(data = vor,fill = NA,
+                                 linetype = 2, alpha = 0.3) +
+        geom_sf(data = segs, aes(col = dfc1$val), linewidth = 3, alpha = 0.7) +
+        geom_text(data = cents, aes(x = X, y = Y, label = round(dfc1$val,3))) + 
+        geom_point(data = nodes, aes(x = x, y = y, col = valc1), size = 10,) +
+        geom_text(data = nodes, aes(x = x, y = y, label = round(valc1, 3))) +
+        theme_void() + ggtitle("C1") + theme(legend.position = "none")
+    ## dataframe G1
+    dfg1 <- data.frame(row = (fm$g1@i + 1), col = (fm$g1@j + 1), val = fm$g1@x)
+    ## remove off diag elements
+    dfg1 <- dfg1[-which(dfg1$row - dfg1$col == 0),]
+    plt_G1 <- ggplot() + geom_sf(data = vor,fill = NA,
+                                 linetype = 2, alpha = 0.3) +
+        geom_sf(data = segs, aes(col = dfg1$val), linewidth = 3, alpha = 0.7) +
+        geom_text(data = cents, aes(x = X, y = Y, label = round(dfg1$val,3))) + 
+        geom_point(data = nodes, aes(x = x, y = y, col = valg1), size = 10,) +
+        geom_text(data = nodes, aes(x = x, y = y, label = round(valg1, 3))) +
+        theme_void() + ggtitle("G1") + theme(legend.position = "none")  
+     patchwork::wrap_plots(plt_C1, plt_G1, nrow = 1) & 
+        scale_fill_continuous(type = "viridis", na.value = NA)
 }
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ## mems
 
-[![codecov](https://codecov.io/gh/cmjt/mems/graph/badge.svg?token=DRFASZHYAU)](https://codecov.io/gh/cmjt/mems)
+Trying to make sense of mesh *influence*.
 
+[![codecov](https://codecov.io/gh/cmjt/mems/graph/badge.svg?token=DRFASZHYAU)](https://codecov.io/gh/cmjt/mems)
 [![stability-experimental](https://img.shields.io/badge/stability-experimental-orange.svg)](https://github.com/joethorley/stability-badges#experimental)
 
 To install run
@@ -12,17 +13,23 @@ devtools::install_github("mems")
 
 ## mesh triangles
 
+Below is an illustration of mesh (Delaunay triangulation) elements. Each triangle (T1, ..., T4) is formed by the connection of three nodes (i.e., three of ni, i = 1,...,6) via an edge. Each node *influence* can be thought of as proportional to the area of the Voronoi cell (coloured polygon) centred at that node. The *influence* of each triangle (T1, ..., T4) on each node (n1, ..., n6) is illustrated by each coloured line segment (i.e., the *length* of the edged connected to each node within the surrounding Voronoi cell).
+
+
 ![](https://github.com/cmjt/mems/blob/main/docs/example_mesh_attributes.png?raw=true)
 
-Illustration of mesh (Delaunay triangulation) elements. Each triangle (T1, ..., T4) is formed by the connection of three nodes (i.e., three of ni, i = 1,...,6) via an edge. Each node *influence* can be thought of as proportional to the area of the Voronoi cell (coloured polygon) centred at that node. The *influence* of each triangle (T1, ..., T4) on each node (n1, ..., n6) is illustrated by each coloured line segment (i.e., the *length* of the edged connected to each node within the surrounding Voronoi cell).
-
+<details>
+  <summary>Plot code</summary>
+  
 ```r
-require(mems)
-data(example_mesh, package = "mems")
-mesh <- example_mesh
+## packages & data
 require(deldir)
 require(sf)
 require(ggplot2)
+require(mems)
+data(example_mesh, package = "mems")
+mesh <- example_mesh
+## manipulation
 nodes <- data.frame(x = mesh$loc[,1], y = mesh$loc[,2])
 tesselation <- deldir(nodes$x, nodes$y)
 tiles <- tile.list(tesselation) ## voronoi diagram (extends outside boundary)
@@ -32,13 +39,15 @@ lin_sf <- half_segments(mesh)
 col_vor <- RColorBrewer::brewer.pal(6,"Dark2") 
 col_lin_sf <- col_vor[(st_intersection(vor, lin_sf) %>%
     subset(., st_geometry_type(st_geometry(.)) == "LINESTRING"))$id]
-## main plot
+## plot
 ggplot() +  geom_sf(data = deldir_2_sf(tiles),fill = col_vor,
                     linetype = 2, alpha = 0.3) +
     geom_sf(data = sf, fill = NA, linewidth = 1) +
     geom_sf(data = lin_sf, col = col_lin_sf, linewidth = 2, alpha = 0.5) + 
-    theme_void() + geom_text(data = cens(mesh), aes(x = x, y = y, label = triangle),
-                             size = 7) +
+    theme_void() + geom_text(data = cens(mesh), aes(x = x, y = y,
+    label = triangle),size = 7) +
     geom_point(data = nodes, aes(x = x, y = y), size = 10, col = col_vor) +
     geom_text(data = nodes, aes(x = x, y = y, label = paste("n", 1:nrow(nodes))))
-```		   
+```
+
+</details>
